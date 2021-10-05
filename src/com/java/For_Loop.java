0632f7e85aa1de5c0159bc70f48cb66c5004b794
Muthukumar2012 <rmuthukumar32012@gmail.com>
2021-10-06 03:10:29 +0530

    Done

---
+++ src/com/java/For_Loop.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class For_Loop {
+	public static void main(String[] args) {
+		for (int x = 13; x < 18; x++) {
+			System.out.println(x);
+		}
+		for (int z = 18; z >= 13; z--) {
+			System.out.println(z);
+		}
+	}
+}
